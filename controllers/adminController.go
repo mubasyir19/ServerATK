b477411af120f691133bb4a0e5e9f878c9e0af54
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-09-03 11:24:36 +0700

    Create structure code signin & signup admin

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -1,1 +1,25 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func SignUpAdmin(c *fiber.Ctx) {
+	// request body
+
+	// Hashing password
+
+	// kirim ke dbase
+
+	// respon
+}
+
+func SignInAdmin(c *fiber.Ctx) {
+	// request body (username & password)
+
+	// Cek data user (dari body username)
+
+	// Compare hashPass dengan pass
+
+	// Generate jwt token
+
+	// respon token
+}
