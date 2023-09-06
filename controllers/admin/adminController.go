ffb46aabe5e7d196152cbcd7c00fbd5c128bb1b5
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-09-07 00:01:41 +0700

    Create controller getall products

--- controllers/admin/adminController.go
+++ controllers/admin/adminController.go
@@ -1,1 +1,33 @@
 package admin
+
+import (
+	"ServerATK/database"
+	"ServerATK/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetAllProducts(c *fiber.Ctx) error {
+	db := database.DB
+
+	var products []models.Product
+
+	result := db.Find(&products)
+	if result.Error != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed save data to database",
+			"error":   result.Error.Error(),
+		})
+	}
+
+	if len(products) == 0 {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "No products found in the database",
+		})
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success save data to database",
+		"data":    result,
+	})
+}
