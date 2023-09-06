fdfcf6962b4af96fe98b43b2468ce42cba4e5671
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-09-06 23:46:37 +0700

    Cleaning

--- routes/route.go
+++ routes/route.go
@@ -1,21 +1,10 @@
 package routes
 
 import (
-	"ServerATK/controllers"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetUpRoute(router fiber.Router) {
 	route := router.Group("/api")
 
-	route.Get("/test", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
-			"message": "Hello World",
-		})
-	})
-
-	route.Get("/products", controllers.GetAllProducts)
-	route.Get("/categories", controllers.GetAllCategories)
-	route.Post("/add/category", controllers.AddCategory)
 }
