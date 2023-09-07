fa69abff554907f207632e23594a1b88b0eaff4e
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-09-08 00:33:21 +0700

    Migrate database to postgresql

--- database/connect.go
+++ database/connect.go
@@ -3,7 +3,7 @@ package database
 import (
 	"fmt"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -12,8 +12,8 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 
-	dsn := "root:@tcp(127.0.0.1:3306)/db_atk?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "host=localhost user=postgres password=mahdy19 dbname=db_atk port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed connect to Database")
 	}
--- models/model.go
+++ models/model.go
@@ -7,60 +7,59 @@ import (
 	"gorm.io/gorm"
 )
 
-
 type User struct {
 	gorm.Model
-	ID 				uuid.UUID
-	Username 		string
-	Password 		string
-	FullName 		string
-	Address 		string
-	PhoneNumber 	string
-	Orders 			[]Order `gorm:"foreignKey:UserID"`
+	ID          uuid.UUID
+	Username    string
+	Password    string
+	FullName    string
+	Address     string
+	PhoneNumber string
+	Orders      []Order `gorm:"foreignKey:UserID"`
 }
 
 type Category struct {
 	gorm.Model
-	ID 			uuid.UUID
-	Name 		string
-	Products	[]Product `gorm:"foreignKey:CategoryID"`
+	ID       uuid.UUID
+	Name     string
+	Products []Product `gorm:"foreignKey:CategoryID"`
 }
 
 type Product struct {
 	gorm.Model
-	ID 				uuid.UUID
-	Name 			string
-	Description 	string
-	Price 			float64
-	Stock 			int64
-	Photo 			string
-	CategoryID 		uint
+	ID          uuid.UUID
+	Name        string
+	Description string
+	Price       float64
+	Stock       int64
+	Photo       string
+	CategoryID  uuid.UUID
 }
 
 type Order struct {
 	gorm.Model
-	ID         		uuid.UUID
-	UserID     		uint
-	Date    		time.Time
-	TotalPrice 		float64
-	AlamatKirim 	string
-	OrderDetails 	[]OrderDetail `gorm:"foreignKey:OrderID"`
+	ID           uuid.UUID
+	UserID       uuid.UUID
+	Date         time.Time
+	TotalPrice   float64
+	AlamatKirim  string
+	OrderDetails []OrderDetail `gorm:"foreignKey:OrderID"`
 }
 
 type OrderDetail struct {
 	gorm.Model
-	ID          	uuid.UUID
-	OrderID     	uint
-	ProductID    	uint
-	Quantity      	int64
-	UnitPrice 		int64
-	SubTotal    	int64
+	ID        uuid.UUID
+	OrderID   uuid.UUID
+	ProductID uuid.UUID
+	Quantity  int64
+	UnitPrice int64
+	SubTotal  int64
 }
 
 type Cart struct {
 	gorm.Model
-	ID     	uuid.UUID
-	UserID 	uint
+	ID     uuid.UUID
+	UserID uuid.UUID
 }
 
 type StatusBayar string
@@ -73,8 +72,8 @@ const (
 
 type Payment struct {
 	gorm.Model
-	ID 			uuid.UUID
-	TotalPay   	float64
-	PayDate 	time.Time
-	Status		StatusBayar
+	ID       uuid.UUID
+	TotalPay float64
+	PayDate  time.Time
+	Status   StatusBayar
 }
