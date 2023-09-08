5e719449989f80df8add0d90c360c4779b954bd9
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-09-09 00:35:53 +0700

    update(adminController, getAll, product): add preload category

--- controllers/admin/adminController.go
+++ controllers/admin/adminController.go
@@ -124,7 +124,7 @@ func GetAllProducts(c *fiber.Ctx) error {
 
 	var products []models.Product
 
-	result := db.Find(&products)
+	result := db.Preload("Category").Find(&products)
 	if result.Error != nil {
 		return c.Status(500).JSON(fiber.Map{
 			"message": "Failed save data to database",
